11ccf118a7da0276c76ab1f576708e7448278348
NITC2 <126461340+NITC2@users.noreply.github.com>
2023-02-27 12:50:26 +0000

    Update alembic_instruction.py

--- magnumopus/resources/alembic_instruction.py
+++ magnumopus/resources/alembic_instruction.py
@@ -15,7 +15,7 @@ substance_schema = SubstanceSchema()
 class AlembicInstructionResource(Resource):
     def get(self):
         """This should return past requests/commands."""
-        pass
+        return "Bonjour!"
 
     def post(self):
         """
